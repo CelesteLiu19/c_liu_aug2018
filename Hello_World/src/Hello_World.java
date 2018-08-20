138a3797dceda6e21524f0cb248f1ebefe62e4be
CelesteLiu19 <19liuc56@stu.smuhsd.org>
2018-08-20 09:47:02 -0700

    First project to commit to GitHub for 2018!

---
+++ Hello_World/src/Hello_World.java
@@ -0,0 +1,15 @@
+/**
+ * Write a description of class Hello_world here.
+ * 
+ * @author Celeste Liu 
+ * @version 8/20/18
+ */
+public class Hello_World
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World! From Celeste Liu"); //Display the string
+    }
+}
+
+
